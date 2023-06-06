d93c94ed252bfec886618b458bcf323698e75ee7
Catherine <131923975+catherine-ni@users.noreply.github.com>
2023-06-06 14:33:28 -0700

    Add files via upload

--- SQL Nashville Pt.1.sql
+++ SQL Nashville Pt.1.sql
@@ -50,3 +50,61 @@ join PortfolioProgect..nashvillehousing b
 on a.parcelid = b.parcelid
 and a.uniqueid <> b.uniqueid
 where a.propertyaddress is null
+
+--Breaking out Address into Individual Colums (Address, City, State)
+
+Select  SUBSTRING(propertyaddress, 1, CHARINDEX(',', propertyaddress) -1 ) as StreetAddress,
+SUBSTRING(propertyaddress, charindex(',', propertyaddress) +1 , Len(propertyaddress)) as City
+from PortfolioProgect..NashvilleHousing
+
+--Update table becasue you can't separate an already existing colum into 2, you need to make 2 new colums
+
+Alter table NashvilleHousing
+add StreetAddress nvarchar(255), City nvarchar(255)
+
+Update nashvillehousing
+set StreetAddress = SUBSTRING(propertyaddress, 1, CHARINDEX(',', propertyaddress) -1 ),
+City = SUBSTRING(propertyaddress, charindex(',', propertyaddress) +1 , Len(propertyaddress))
+
+--Easier way to do it is PARSENAME
+
+Select OwnerAddress
+from PortfolioProgect..NashvilleHousing
+
+select Parsename (Replace(owneraddress, ',', '.'),3) as Street,
+Parsename (Replace(owneraddress, ',', '.'),2) as City,
+Parsename (Replace(owneraddress, ',', '.'),1) as State
+from PortfolioProgect..NashvilleHousing
+
+Alter table NashvilleHousing
+add OwnerStreetAddress nvarchar(255), OwnerCity nvarchar(255), OwnerState nvarchar(255)
+
+Update nashvillehousing
+set OwnerStreetAddress = Parsename (Replace(owneraddress, ',', '.'),3) as Street,
+OwnerCity = Parsename (Replace(owneraddress, ',', '.'),2) as City,
+OwnerState = Parsename (Replace(owneraddress, ',', '.'),1) as State
+
+--Change Y and N to Yes and No
+
+select Distinct(SoldAsVacant), count(soldasvacant)
+from PortfolioProgect..NashvilleHousing
+group by SoldAsVacant
+order by 2
+
+select SoldAsVacant,
+case
+  when soldasvacant = 'Y' then 'Yes'
+  when soldasvacant = 'N' then 'No'
+  Else Soldasvacant
+end
+from PortfolioProgect..NashvilleHousing
+group by SoldAsVacant
+
+update PortfolioProgect..NashvilleHousing
+set soldasvacant = case
+  when soldasvacant = 'Y' then 'Yes'
+  when soldasvacant = 'N' then 'No'
+  Else Soldasvacant
+end
+
+--Remove Duplicates
